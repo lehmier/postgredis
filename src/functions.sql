4ebf494e2e4c0dff1ea014d40f7fea951465bbc0
lehmier <jesse@lehmier.com>
2021-09-06 20:39:58 -0400

    Change functions to sql

--- src/functions.sql
+++ src/functions.sql
@@ -20,28 +20,22 @@ $$ language plpgsql;
  * PING [message]
  * https://redis.io/commands/ping
  */
-create function redis.ping(message varchar default null) returns varchar as $$
-begin
-    return redis.exec('PING' || case when message is null then '' else ' ' || message end);
-end;
-$$ language plpgsql;
+create or replace function redis.ping(message varchar default null) returns varchar as $$
+    select redis.exec('PING' || case when message is null then '' else ' ' || message end);
+$$ language sql;
 
 /**
  * SET key value [EX seconds|PX milliseconds|EXAT timestamp|PXAT milliseconds-timestamp|KEEPTTL] [NX|XX] [GET] 
  * https://redis.io/commands/set
  */
-create function redis.set(key varchar, value varchar, expiry varchar default '') returns varchar as $$
-begin
-    return redis.exec('SET ' || key || ' ' || value || ' ' || expiry);
-end;
-$$ language plpgsql;
+create or replace function redis.set(key varchar, value varchar, expiry varchar default '') returns varchar as $$
+    select redis.exec('SET ' || key || ' ' || value || ' ' || expiry);
+$$ language sql;
 
 /**
  * GET key
  * https://redis.io/commands/get
  */
-create function redis.get(key varchar) returns varchar as $$
-begin
-    return redis.exec('GET ' || key);
-end;
-$$ language plpgsql;
+create or replace function redis.get(key varchar) returns varchar as $$
+    select redis.exec('GET ' || key);
+$$ language sql;
